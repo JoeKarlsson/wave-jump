c8820ebcf318bed62135a6f8bc94ddea1e5afaba
Joe Karlsson <josephcarlson1@gmail.com>
2018-01-27 14:32:33 -0600

    music works.

--- src/states/Splash.js
+++ src/states/Splash.js
@@ -17,6 +17,11 @@ export default class extends Phaser.State {
   }
 
   create () {
+    // Game Music
+    let music = this.game.add.audio('Ferrari')
+    music.loopFull(0.6)
+    music.play()
+
     this.spaceBar = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
   }
 
