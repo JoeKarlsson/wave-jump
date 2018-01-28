0df8ddb6262a2e8f6861a2758ff83629d1541a56
Cal Carlson <calcarlson@gmail.com>
2018-01-28 16:05:47 -0600

    bug fixes

--- src/states/Game.js
+++ src/states/Game.js
@@ -53,7 +53,6 @@ export default class GameState extends Phaser.State {
   }
 
   initRaceGate () {
-
     this.raceGate = this.add.sprite(window.innerWidth * window.devicePixelRatio - this.game.rnd.between(200, 600), this.game.height - this.game.rnd.between(200, 600), 'raceGate')
     this.raceGate.scale.setTo(this.game.scaleRatio, this.game.scaleRatio)
     this.raceGate.y += this.raceGate.height
