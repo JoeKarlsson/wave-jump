e58abf3ab8cf866a6a3461b34c6e7037e63f3cd5
Daniel Olson <olso7118@umn.edu>
2018-01-28 01:28:07 -0600

    Created a much more realistic wave on the verge of rebounding off wave made dummy player classes to help determine angles

--- src/states/Boot.js
+++ src/states/Boot.js
@@ -35,7 +35,7 @@ export default class extends Phaser.State {
     this.load.image('loaderBg', './assets/images/loader-bg.png')
     this.load.image('loaderBar', './assets/images/loader-bar.png')
     this.load.image('background', './assets/images/alt-game-background.png')
-    this.load.image('tempWave', './assets/images/waveTemp.png')
+    this.load.image('tempWave', './assets/images/wave prototype.png')
     this.load.image('raceGate', './assets/images/1 cropped.png')
     this.game.load.audio('Ferrari', 'assets/audio/Ferrari.wav')
   }
