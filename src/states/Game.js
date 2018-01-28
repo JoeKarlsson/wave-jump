abc548a56f97682b80dd3213c1d2b28ae6d05cfb
Cal Carlson <calcarlson@gmail.com>
2018-01-28 16:23:15 -0600

    bug fix on spawn

--- src/states/Game.js
+++ src/states/Game.js
@@ -53,7 +53,7 @@ export default class GameState extends Phaser.State {
   }
 
   initRaceGate () {
-    this.raceGate = this.add.sprite(this.game.rnd.between(200, 600), this.game.height - this.game.rnd.between(200, 600), 'raceGate')
+    this.raceGate = this.add.sprite(this.game.rnd.between(this.world.width / 3, this.world.width / 1.5), this.game.height - this.game.rnd.between(this.game.height - 400, this.game.height - 100), 'raceGate')
     this.raceGate.scale.setTo(this.game.scaleRatio, this.game.scaleRatio)
     this.raceGate.y += this.raceGate.height
     this.raceGate.anchor.set(0.5, 0.5)
